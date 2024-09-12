85fc79e7ab2139c77825f53d44b16dc5beef0c5c
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2024-09-12 13:47:33 -0600

    Adding data dummy

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from funciones import watson_discovery as wd
 from funciones import pdf_extractor as pe
 
 def main():
-    print("Iniciando el programa...")
+    print("Iniciando extracción de información")
+
+    # Procesar PDF de catálogo
+    pdf = './data/pdf.pdf'
+    output = './data'
+    pe.procesar_pdf(pdf, output)
 
     # Eliminar documentos existentes en la colección Watson Discovery
     wd.eliminar_documentos()
 
     # Subir archivos Excel desde una carpeta a Watson Discovery
-    carpeta_excel = '/Users/sofiadonlucas/Desktop/Visual/NDS/archivos_excel'
+    carpeta_excel = './archivos_dummy'
     wd.subir_archivos_de_carpeta(carpeta_excel)
-
-    # Procesar PDF
-    pdf = './data/pdf.pdf'
-    output = './data'
-    pe.procesar_pdf(pdf, output)
 
 if __name__ == "__main__":
     main()
