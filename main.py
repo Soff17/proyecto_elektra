8fcd7dc217f8f27aa9396b58a7182512c59b69ad
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2024-09-13 13:14:12 -0600

    Merge chances by Javier and Sofia

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from funciones import pdf_extractor as pe
 def main():
     print("Iniciando extracción de información")
 
+    # Eliminar documentos existentes en la colección Watson Discovery
+    wd.eliminar_documentos()
+
     # Procesar PDF de catálogo
     pdf = './data/pdf.pdf'
     output = './data'
     pe.procesar_pdf(pdf, output)
 
-    # Eliminar documentos existentes en la colección Watson Discovery
-    wd.eliminar_documentos()
-
     # Subir archivos Excel desde una carpeta a Watson Discovery
-    carpeta_excel = './archivos_dummy'
+    carpeta_excel = './data'
     wd.subir_archivos_de_carpeta(carpeta_excel)
 
 if __name__ == "__main__":
