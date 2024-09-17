fded3f1a56e09d45a2e118daff0c71bdfd711503
Javier <A01662438@tec.mx>
2024-09-17 17:12:05 -0600

    Extraer las imágenes

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from funciones import watson_discovery as wd
+#from funciones import watson_discovery as wd
 from funciones import pdf_extractor as pe
 
 def main():
     print("Iniciando extracción de información")
 
     # Eliminar documentos existentes en la colección Watson Discovery
-    wd.eliminar_documentos()
+    #wd.eliminar_documentos()
 
     # Procesar PDF de catálogo
     pdf = './data/pdf.pdf'
-    output = './data'
+    output = './archivos_dummy'
     pe.procesar_pdf(pdf, output)
 
     # Subir archivos Excel desde una carpeta a Watson Discovery
-    carpeta_excel = './data'
-    wd.subir_archivos_de_carpeta(carpeta_excel)
+    #carpeta_excel = './data'
+    #wd.subir_archivos_de_carpeta(carpeta_excel)
 
 if __name__ == "__main__":
     main()
