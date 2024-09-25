8c24c213997e0cde17190c9c3062da168e83c7d6
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2024-09-25 08:13:50 -0600

    Token implementation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS  # Importar CORS
-#from funciones import watson_discovery as wd
 from funciones import pdf_extractor as pe
 from funciones import image_storage as st
+from funciones.token import verificar_token, generate_token 
 from dotenv import load_dotenv
 import os
 
@@ -21,22 +21,20 @@ output_imagenes = './imagenes'
 bucket_name = 'nds_test'
 carpeta_en_bucket = 'imagenes_subidas'
 
-# Definición de token
-TOKEN_VALIDO = os.getenv('TOKEN_VALIDO')
+# Endpoint para generar un token dinámico
+@app.route('/generate_token', methods=['GET'])
+def generate_token_endpoint():
+    token = generate_token()
+    return jsonify({"token": token}), 200
 
-# Middleware para verificar el token en cada solicitud
-def verificar_token():
-    token = request.headers.get('Authorization')
-    if not token or token != TOKEN_VALIDO:
-        abort(401, description="Token no válido o faltante")
-
+# Endpoint para procesamiento de documento e imágenes
 @app.route('/new_documents', methods=['POST'])
 def procesar_y_subir():
     try:
         # Verificar el token antes de proceder
         verificar_token()
 
-        # Paso 1: Eliminar documentos
+        # Paso 1: Eliminar documentos (comentar si no es necesario)
         # wd.eliminar_documentos()
         print("Documentos eliminados exitosamente.")
 
@@ -44,15 +42,15 @@ def procesar_y_subir():
         pe.procesar_pdf(pdf, output_arhivos, output_imagenes)
         print("PDF procesado exitosamente.")
 
-        # Paso 3: Subir archivos a Watson Discovery
+        # Paso 3: Subir archivos a Watson Discovery (comentar si no es necesario)
         # wd.subir_archivos_de_carpeta(output_arhivos)
         print("Archivos subidos exitosamente.")
 
-        # Paso 4: Eliminar imágenes existentes en Google Cloud Storage
+        # Paso 4: Eliminar imágenes existentes en Google Cloud Storage (comentar si no es necesario)
         # st.empty_bucket(bucket_name)
         print("Imágenes eliminadas")
         
-        # Paso 5: Subir las imágenes a Google Cloud Storage
+        # Paso 5: Subir las imágenes a Google Cloud Storage (comentar si no es necesario)
         # st.upload_images_in_folder(bucket_name, output_imagenes, carpeta_en_bucket)
         print("Imágenes subidas exitosamente a Google Cloud Storage.")
 
